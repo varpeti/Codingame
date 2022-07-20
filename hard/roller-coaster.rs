7b855de395deb87c4f4b78db7bb9a5c9c47f48b6
varpeti <peti.var97@gmail.com>
2022-07-20 22:47:04 +0200

    roller-coaster 100% (🦀) faster input reading offset for the sequences timing removed

--- hard/roller-coaster.rs
+++ hard/roller-coaster.rs
@@ -1,33 +1,26 @@
-use std::{io::{self}, time::SystemTime};
+use std::{io::{Read}, fs::File, os::unix::prelude::FromRawFd};
 
 macro_rules! parse_input {
     ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
 }
 
-fn next_input() -> String {
-    let mut input_line = String::new();
-    io::stdin().read_line(&mut input_line).unwrap();
-    input_line
-}
-
 fn main() {
     
-    let now = SystemTime::now();
-    
-    let input = next_input();
-    let inputs = input.split(" ").collect::<Vec<_>>();
+    let mut input = String::new();
+    let mut stdin = unsafe { File::from_raw_fd(0) };
+    stdin.read_to_string(&mut input).unwrap();
+    let lines = input.split("\n").collect::<Vec<_>>();
+
+    let inputs = lines[0].split(" ").collect::<Vec<_>>();
     let l = parse_input!(inputs[0], u32);
     let mut c = parse_input!(inputs[1], u32);
-    let n = parse_input!(inputs[2], usize);
+    //let n = parse_input!(inputs[2], usize);
     let mut queue = Vec::<u32>::new();
-    for _ in 0..n { queue.push(parse_input!(next_input(), u32));}
-
-    eprintln!("Read input: {:?}", now.elapsed().unwrap().as_micros());
-
-    let now = SystemTime::now();
+    for i in 1..lines.len()-1 {queue.push(parse_input!(lines[i], u32));}
 
     let mut sequences = Vec::<Vec<u32>>::new(); // Save all possible sequences til it repeates itself (so we trade memory for time gain)
     let mut sequence = Vec::<u32>::new();
+    let mut offset: Option::<usize> = None; // Number of init sequences before repeated sequences.
     let mut overflow = false;
     let mut i: usize = 0; // Where are we at atm in the queue
     loop {
@@ -36,46 +29,56 @@ fn main() {
         loop {
             ride_group += queue[i];
             i+=1; if i==queue.len() { i=0; overflow=true; }
-            if one_seat_for_one_person == i { break; } // Occures when there are less people than the capacity of the ride
-            if ride_group + queue[i] > l { break; } // Next group cannot fit
+            if one_seat_for_one_person == i || ride_group + queue[i] > l { break; }
         }
         sequence.push(ride_group);
         if overflow {
             overflow = false;
+            offset = sequences.iter().position(|s| *s == sequence);
+            if offset != None { break; } // Found a repeate
             sequences.push(sequence.clone());
             sequence.clear();
-            if sequences.len() > 1 && sequences[sequences.len()-1] == sequences[sequences.len()-2] { break; } // Found a repeate
         }
     }
-    //eprintln!("{:?}", sequences);
-
-    eprintln!("Sequence hunt: {:?}", now.elapsed().unwrap().as_micros());
-
-    let now = SystemTime::now();
+    let offset = offset.unwrap();
+    eprintln!("{:?}, {:?}", sequences, offset);
 
     //Before the repeate
     let mut init: u32 = 0;
-    for si in 0..sequences.len()-2 {
+    for si in 0..offset {
         init += sequences[si].iter().sum::<u32>();
         c -= sequences[si].len() as u32;
     }
     
     //Repeate
-    let rseq = sequences.last().unwrap();
-    let repeate: u64 = 
-        (rseq.iter().sum::<u32>() as u64) *
-        ((c as u64)/(rseq.len() as u64));
+    let mut repeate: u64 = 0;
+    let times = c/sequences.iter().skip(offset).map(|s| s.len() as u32).sum::<u32>();
     
+
     //After the repeate (if last does not go a full cycle)
-    let remaining = c as usize % rseq.len();
+    let mut remaining = c as usize % sequences.iter().skip(offset).map(|s| s.len()).sum::<usize>();
+    eprintln!("{:?}, {:?}", c, remaining);
     let mut end: u32 = 0;
-    for i in 0..(remaining) {
-        end += rseq[i];
+
+    
+
+    for si in offset..sequences.len() {
+
+        repeate += sequences[si].iter().sum::<u32>() as u64; //Repeated
+
+        //Remaining
+        if remaining >= sequences[si].len() {
+            end += sequences[si].iter().sum::<u32>();
+            remaining -= sequences[si].len(); 
+        } else {
+            for i in 0..remaining {
+                end += sequences[si][i];
+            }
+            remaining = 0;
+        }
     }
 
-    eprintln!("Cacl: {:?}", now.elapsed().unwrap().as_micros());
-
-    //eprintln!("{} {} {}", init, repeate, end);
-    println!("{}", init as u64 + repeate + end as u64);
+    eprintln!("{}, {} * {}, {}", init, repeate, times, end);
+    println!("{}", init as u64 + (repeate * times as u64) + end as u64);
 }
 
