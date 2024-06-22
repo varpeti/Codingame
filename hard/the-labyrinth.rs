c66b6d09bce2802a91111ef1c097cd6c8fbb5ce5
varpeti <peti.var97@gmail.com>
2024-06-22 21:21:21 +0200

    the-labyrinth.rs (🦀) (astar, loop) 100%

--- hard/the-labyrinth.rs
+++ hard/the-labyrinth.rs
@@ -1,41 +1,78 @@
-use std::{io, fmt};
+use std::{
+    cmp::Ordering,
+    collections::{BinaryHeap, HashMap, HashSet},
+    fmt,
+    hash::Hash,
+};
 
-/*
-macro_rules! r { // Reads all
-    ($l:expr) => {
-        let mut input = String::new();
-        let mut stdin = unsafe { File::from_raw_fd(0) };
-        stdin.read_to_string(&mut input).unwrap();
-        $l = input.split("\n");
-    };
-}*/
-
-macro_rules! rs { // Reads a single line
+use std::io;
+macro_rules! rs {
+    // Reads a single line
     ($l:expr) => {
         let mut s = String::new();
         io::stdin().read_line(&mut s).unwrap();
-        $l = s.split("\n"); //Dirty way to turn into an iter...
+        $l = s.split("\n"); // Dirty way to turn into an iter...
     };
 }
 
-macro_rules! p {
-    ($x:expr) => ($x.next().unwrap().trim());
-    ($x:expr, $t:ident) => (p!($x).parse::<$t>().unwrap());
-    ($x:expr, $s:expr) => (p!($x).split($s));
+macro_rules! p{
+    ($x:expr)=>($x.next().unwrap().trim());
+    ($x:expr,$($t:ty),*)=>{($(p!($x).parse::<$t>().unwrap()),*)};
+    ($x:expr,$s:expr)=>(p!($x).split($s));
+}
+
+fn main() {
+    let mut l;
+    rs!(l);
+    let mut i = p!(l, " ");
+    let (number_of_rows, number_of_columns, _alarm_time) = p!(i, usize, usize, usize);
+
+    let mut maze = Maze::new(number_of_rows, number_of_columns);
+    let mut state = State::Explore;
+    loop {
+        let mut l;
+        rs!(l);
+        let mut i = p!(l, " ");
+        let rick = p!(i, usize, usize);
+        let rick = Pos::new(rick.1, rick.0);
+
+        // Update maze
+        for my in 0..number_of_rows {
+            let mut l;
+            rs!(l);
+            for (mx, c) in p!(l).chars().enumerate() {
+                maze.set(mx, my, &c);
+            }
+        }
+        // Update state if ControlRoom is reached
+        if maze.get(rick.x, rick.y).t == NodeType::ControlRoom {
+            state = State::ToTheEnd;
+        }
+
+        eprintln!("{:#?}", maze);
+        println!("{}", get_direction(&maze, &rick, &mut state));
+    }
 }
 
 #[derive(Clone, PartialEq, Eq, Debug)]
-enum NodeType {Start, ControlRoom, Space, Wall, Unkown}
+enum NodeType {
+    Start,
+    ControlRoom,
+    Space,
+    Wall,
+    Unkown,
+}
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 struct Node {
     t: NodeType,
 }
 
-
 impl Node {
     fn new(c: &char) -> Node {
-        let mut node = Node{t: NodeType::Unkown};
+        let mut node = Node {
+            t: NodeType::Unkown,
+        };
         node.t = match c {
             'T' => NodeType::Start,
             'C' => NodeType::ControlRoom,
@@ -64,27 +101,33 @@ struct Maze {
 
 impl Maze {
     fn new(r: usize, c: usize) -> Maze {
-        Maze{maze: vec![vec![Node::new(&'?'); c]; r]}
+        Maze {
+            maze: vec![vec![Node::new(&'?'); c]; r],
+        }
     }
 
     fn set(&mut self, x: usize, y: usize, new_c: &char) -> &Self {
         let new_c = Node::new(new_c);
         if let Some(row) = self.maze.get_mut(y) {
             if let Some(c) = row.get_mut(x) {
-                if c.t == NodeType::Unkown {c.t = new_c.t; return self;}
-                if c.t != new_c.t {eprintln!("Error? {} {} {:?} {:?}", x, y, c, new_c)}
+                if c.t == NodeType::Unkown {
+                    c.t = new_c.t;
+                    return self;
+                }
+                if c.t != new_c.t {
+                    eprintln!("Error? {} {} {:?} {:?}", x, y, c, new_c)
+                }
             }
         }
         self
     }
-    
+
     fn get(&self, x: usize, y: usize) -> Node {
         if let Some(row) = self.maze.get(y) {
             if let Some(c) = row.get(x) {
                 return c.clone();
             }
         }
-        eprintln!("Error? {} {}", x, y);
         Node::new(&'?')
     }
 }
@@ -98,48 +141,161 @@ impl fmt::Debug for Maze {
                 res.push(c.as_char())
             }
             res.push('\n');
-        };
+        }
         write!(f, "{}", res)
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
+enum State {
+    Explore,
+    ToControl,
+    ToTheEnd,
+}
 
-fn todo_remove_me_solve(maze: &Maze, rx: usize, ry: usize, todo_remove_me_state: &mut bool) -> String
+fn get_direction(maze: &Maze, rick: &Pos, state: &mut State) -> String {
+    let goal = match state {
+        State::Explore => NodeType::Unkown,
+        State::ToControl => NodeType::ControlRoom,
+        State::ToTheEnd => NodeType::Start,
+    };
+
+    if let Some(path) = astar(
+        rick,
+        |pos| get_neighs(pos, maze),
+        |_| 0, // TODO heuristic
+        |pos| maze.get(pos.x, pos.y).t == goal,
+    ) {
+        eprintln!("{:?}", path);
+        let next = &path[path.len() - 2]; // -1 is where rick is
+        return (match (
+            next.x as isize - rick.x as isize,
+            next.y as isize - rick.y as isize,
+        ) {
+            (-1, 0) => "LEFT",
+            (1, 0) => "RIGHT",
+            (0, -1) => "UP",
+            (0, 1) => "DOWN",
+            (dx, dy) => panic!(format!("{} {}", dx, dy)),
+        })
+        .to_string();
+    }
+    // No other Unkown room is reachable
+    *state = State::ToControl;
+    get_direction(maze, rick, state)
+}
+
+fn get_neighs(pos: &Pos, maze: &Maze) -> Vec<(Pos, usize)> {
+    vec![
+        (Pos::new(pos.x + 1, pos.y), 1_usize),
+        (Pos::new(pos.x - 1, pos.y), 1_usize),
+        (Pos::new(pos.x, pos.y + 1), 1_usize),
+        (Pos::new(pos.x, pos.y - 1), 1_usize),
+    ]
+    .into_iter()
+    .filter(|(pos, _)| maze.get(pos.x, pos.y).t != NodeType::Wall)
+    .collect()
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Pos {
+    x: usize,
+    y: usize,
+}
+
+impl Pos {
+    fn new(x: usize, y: usize) -> Self {
+        Pos { x, y }
+    }
+}
+
+fn astar<N, FN, FH, FS>(
+    start_node: &N,
+    mut successors: FN,
+    mut heuristic: FH,
+    mut success: FS,
+) -> Option<Vec<N>>
+where
+    N: Eq + Hash + Clone,
+    FN: FnMut(&N) -> Vec<(N, usize)>,
+    FH: FnMut(&N) -> usize,
+    FS: FnMut(&N) -> bool,
 {
-    if *todo_remove_me_state { return "LEFT".to_string();}
-    if maze.get(rx+1, ry).t != NodeType::Wall {
-        return "RIGHT".to_string();
-    }
-    *todo_remove_me_state = true;
-    "LEFT".to_string()
-}
+    let mut priority_queue = BinaryHeap::<AstarNode<N>>::new();
+    let mut cost_and_parent = HashMap::<N, (usize, Option<N>)>::new();
+    let mut visited = HashSet::<N>::new();
 
-fn main() {
-    let mut l; rs!(l);
-    let mut i = p!(l, " ");
-    let r = p!(i, usize); // number of rows.
-    let c = p!(i, usize); // number of columns.
-    let a = p!(i, i32); // number of rounds between the time the alarm countdown is activated and the time the alarm goes off.
+    priority_queue.push(AstarNode::new(start_node.clone(), 0, 0));
+    cost_and_parent.insert(start_node.clone(), (0, None));
 
-    let mut maze = Maze::new(r, c);
-
-    let mut todo_remove_me_state = false;
-    
-    // game loop
-    loop {
-        let mut l; rs!(l);
-        let mut i = p!(l, " ");
-        let ry = p!(i, usize); // row where Rick is located.
-        let rx = p!(i, usize); // column where Rick is located.
-        for my in 0..r {
-            let mut l; rs!(l);
-            for (mx, c) in p!(l).chars().enumerate() {
-                maze.set(mx, my, &c);
-            }
+    while let Some(current) = priority_queue.pop() {
+        if visited.contains(&current.node) {
+            continue;
         }
-        eprintln!("{:#?}", maze);
-        
-        println!("{}", todo_remove_me_solve(&maze, rx, ry, &mut todo_remove_me_state));
+        if success(&current.node) {
+            let mut path = Vec::<N>::new();
+            let mut current = current.node;
+            while let Some((_, Some(parent))) = cost_and_parent.get(&current) {
+                path.push(current);
+                current = parent.clone();
+            }
+            path.push(start_node.clone());
+            return Some(path);
+        }
+        visited.insert(current.node.clone());
+        for (successor, move_cost) in successors(&current.node) {
+            if visited.contains(&successor) {
+                continue;
+            }
+            let tentative_cost = current.cost + move_cost;
+            if tentative_cost
+                > cost_and_parent
+                    .get(&successor)
+                    .unwrap_or(&(usize::MAX, None))
+                    .0
+            {
+                continue;
+            }
+            priority_queue.push(AstarNode::new(
+                successor.clone(),
+                tentative_cost,
+                tentative_cost + heuristic(&successor),
+            ));
+            cost_and_parent.insert(
+                successor.clone(),
+                (tentative_cost, Some(current.node.clone())),
+            );
+        }
+    }
+
+    None
+}
+
+#[derive(Clone, PartialEq, Eq)]
+struct AstarNode<N> {
+    node: N,
+    cost: usize,
+    priority: usize,
+}
+
+impl<N> AstarNode<N> {
+    fn new(node: N, cost: usize, priority: usize) -> Self {
+        AstarNode {
+            node,
+            cost,
+            priority,
+        }
     }
 }
 
+impl<N: Eq> Ord for AstarNode<N> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.priority.cmp(&self.priority)
+    }
+}
+
+impl<N: Eq> PartialOrd for AstarNode<N> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
